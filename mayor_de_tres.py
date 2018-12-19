b519dd779711d71b1255217755655ce1ecb83980
catrielzz <estradayoel@gmail.com>
2018-12-18 22:05:33 -0300

    wea

---
+++ mayor_de_tres.py
@@ -0,0 +1,7 @@
+first_number = int(input("First number: "))
+second_number = int(input("Second number: "))
+third_number = int(input("Third number: "))
+
+result = max(first_number, second_number, third_number)
+
+print("The biggest number is {} ".format(result))
