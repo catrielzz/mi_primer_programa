0ec2b7e2b3724cf657432ca595b3b3bcee69af88
catrielzz <estradayoel@gmail.com>
2018-12-23 20:10:46 -0300

    Listas y for

---
+++ ejemplo_for.py
@@ -0,0 +1,17 @@
+mi_lista = []
+input_usuario = ""
+
+input_usuario = input("Que necesitas comprar? (Escribe FIN para salir): ")
+
+while input_usuario != "FIN":
+    mi_lista.append(input_usuario)
+    input_usuario = input("Que necesitas comprar? (Escribe FIN para salir): ")
+
+
+largo_lista = len(mi_lista)
+indice_actual = 0
+
+for item in mi_lista:
+    print("Tengo que comprar: {}".format(item))
+
+print("Esta es la lista de la compra, la cantidad de elementos que hay en la lista es {}".format(len(mi_lista)))
