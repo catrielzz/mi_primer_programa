5122bdc287c9b6f7fed4a497b0e7d7360972e9bd
Yoel Estrada Thompson <estradayoel@gmail.com>
2019-05-15 15:17:32 -0300

    more changes

--- ejemplos/contacts.py
+++ ejemplos/contacts.py
@@ -1,8 +1,24 @@
+import pickle
+from time import sleep
+
 ACTION_ADD_CONTACT = 1
 ACTION_REMOVE_CONTACT = 2
 ACTION_FIND_CONTACT = 3
 ACTION_EXPORT_CONTACT = 4
 ACTION_EXIT = 5
+
+SAVE_FILE_NAME = "contacts.save"
+
+MENU_OPTIONS = [ACTION_ADD_CONTACT, ACTION_REMOVE_CONTACT, ACTION_FIND_CONTACT, ACTION_EXPORT_CONTACT, ACTION_EXIT]
+
+
+def ask_until_option_expected(options):
+    selected_action = ""
+
+    while not selected_action.isdigit() or (selected_action.isdigit() and int(selected_action) not in options):
+        selected_action = input("Que opcion deseas? ")
+
+    return int(selected_action)
 
 
 def show_menu():
@@ -12,38 +28,99 @@ def show_menu():
     print("3 - Buscar contacto")
     print("4 - Exportar contactos a un CSV")
     print("5 - Salir")
-    return
+    return ask_until_option_expected(MENU_OPTIONS)
 
 
-def add_contact():
-    pass
+def add_contact(contacts):
+    print("\n\nAñadir contacto\n")
+    contact = {
+        "name": input("Nombre: "),
+        "phone": input("Teléfono: "),
+        "email": input("Email: ")
+    }
+    contacts.append(contact)
+    print("Se ha añadido el contacto {} correctamente\n".format(contact["name"]))
+    sleep(1)
 
 
-def remove_contact():
-    pass
+def remove_contact(contacts):
+    print("\n\nEliminar contacto\n")
+    contact = input("Que contacto deseas eliminar?")
+    for element in contacts:
+        if contact.upper() == element["name"].upper():
+            contacts.remove(element)
+            print("Se ha eliminado contacto {} correctamente".format(contact))
+        else:
+            print("Contacto inválido")
+    sleep(1)
 
 
 def export_contact():
     pass
 
 
-def find_contact():
-    pass
+def find_contact(contacts):
+    print("\n\nBuscar contacto\n")
+    search_term = input("Introducir el nombre del contacto o parte de el, o no ingrese nada para ver la lista de "
+                        "contactos: ")
+    found_contacts = []
+
+    print("He encontrado los siguientes contactos: ")
+    contact_indexes = []
+    contact_counter = 0
+
+    for contact in contacts:
+        if contact["name"].find(search_term) >= 0:
+            found_contacts.append(contact)
+            print("{} - {}".format(contact_counter, contact["name"]))
+            contact_indexes.append(contact_counter)
+            contact_counter += 1
+
+    contact_index = 0
+
+    if len(contact_indexes) > 1:
+        contact_index = ask_until_option_expected(contact_indexes)
+    elif len(contact_indexes) == 0:
+        print("No se ha encontrado ninguno.")
+        return
+
+    print("\nInformacion sobre {}\n".format(found_contacts[contact_index]["name"]))
+    print("Nombre: {name}\nTelefono: {phone}\nEmail: {email}".format(**found_contacts[contact_index]))
+    sleep(2)
+
+
+def load_contacts():
+    try:
+        return pickle.load(open(SAVE_FILE_NAME, "rb"))
+    except FileNotFoundError:
+        return []
+
+
+def save_contacts(contacts):
+    with open(SAVE_FILE_NAME, "wb") as save_file:
+        pickle.dump(contacts, save_file)
+
+    print("Datos guardados correctamente.")
 
 
 def main():
+    contacts = load_contacts()
     action = show_menu()
+
     while action != ACTION_EXIT:
         if action == ACTION_ADD_CONTACT:
-            add_contact()
+            add_contact(contacts)
         elif action == ACTION_REMOVE_CONTACT:
-            remove_contact()
+            remove_contact(contacts)
         elif action == ACTION_EXPORT_CONTACT:
-            export_contact()
+            export_contact(contacts)
         elif action == ACTION_FIND_CONTACT:
-            find_contact()
+            find_contact(contacts)
         action = show_menu()
+        
+    save_contacts(contacts)
+    print("Adios!")
 
 
-if __name__ == "  main  ":
+if __name__ == "__main__":
     main()
